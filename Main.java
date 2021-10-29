b516613ddf00ca18ea4c606d30f157c6fb54cc3b
vrindajadwani <68802198+vrindajadwani@users.noreply.github.com>
2021-10-30 01:17:38 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,33 @@
-public class Main {
-    static int binarySearchIterative(int arr[],int n, int key){
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(arr[mid]==key)
-                return mid;
-            else if(arr[mid]>key)
-                high = mid-1;
-            else
-                low=mid+1;
+public class Main
+{
+    public static int findMajority(int[] nums)
+    {
+        int count = 0, candidate = -1;
+        for (int index = 0; index < nums.length; index++) {
+            if (count == 0) {
+                candidate = nums[index];
+                count = 1;
+            }
+            else {
+                if (nums[index] == candidate)
+                    count++;
+                else
+                    count--;
+            }
         }
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] == candidate)
+                count++;
+        }
+        if (count > (nums.length / 2))
+            return candidate;
         return -1;
     }
-    static int binarySearchRecursive(int arr[],int low, int high,int key){
-        if(low>high)
-            return -1;
-        int mid = low + (high-low)/2;
-        if(arr[mid]==key)
-            return mid;
-        if(arr[mid]>key)
-            return binarySearchRecursive(arr,low,mid-1,key);
-        return binarySearchRecursive(arr,mid+1,high,data);
-    }
-    public static void main(String[] args) {
-        int key=5;
-        int arr[] = {4,6,1,4,8,5,10,9};
-        int n = arr.length;
-        System.out.println("Key found at : " + binarySearchIterative(arr,n,key));
-        System.out.println("Key found at : " + binarySearchRecursive(arr,n,key));
+    public static void main(String[] args)
+    {
+        int arr[] = { 1, 2, 1, 6, 2, 3, 4,1,1};
+        int majority = findMajority(arr);
+        System.out.println(" The majority element is : "
+                + majority);
     }
 }
